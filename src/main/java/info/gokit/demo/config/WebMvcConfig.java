35c71b23103ccad74ed4f999e3587c83e65b0e0d
llitfkitfk <llitfkitfk@gmail.com>
2017-10-25 19:20:59 +0800

    add monitor

--- src/main/java/info/gokit/demo/config/WebMvcConfig.java
+++ src/main/java/info/gokit/demo/config/WebMvcConfig.java
@@ -1,13 +1,19 @@
 package info.gokit.demo.config;
 
 
+import net.bull.javamelody.MonitoringFilter;
+import net.bull.javamelody.SessionListener;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.DispatcherType;
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Bean
     public OpenEntityManagerInViewFilter securityOpenEntityManagerInViewFilter() {
@@ -22,4 +28,21 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         osivFilter.setEntityManagerFactoryBeanName("ordersEntityManagerFactory");
         return osivFilter;
     }
+
+    @Bean
+    public FilterRegistrationBean<MonitoringFilter> javamelodyFilterBean() {
+        FilterRegistrationBean<MonitoringFilter> registration =new FilterRegistrationBean<>();
+        registration.setFilter(new MonitoringFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("javamelodyFilter");
+        registration.setAsyncSupported(true);
+        registration.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC);
+        return registration;
+    }
+
+    @Bean(name = "javamelodySessionListener")
+    public ServletListenerRegistrationBean<SessionListener> sessionListener() {
+        return new ServletListenerRegistrationBean<>(new SessionListener());
+    }
+
 }
